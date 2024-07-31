5b09c03615e3b64526f7d6bb1147afcb0b58b2bf
peter6_huang <peter6_huang@pegatroncorp.com>
2024-07-31 13:43:32 +0800

    test

--- javaScript.js
+++ javaScript.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Load saved data
     const savedData = JSON.parse(localStorage.getItem('bookkeepingData')) || [];
-    savedData.forEach((item, index) => {
-        addItemToList(item, index);
+    savedData.forEach((item) => {
+        addItemToList(item);
     });
 
     form.addEventListener('submit', function(event) {
@@ -20,47 +20,14 @@ document.addEventListener('DOMContentLoaded', function() {
         savedData.push(newItem);
         localStorage.setItem('bookkeepingData', JSON.stringify(savedData));
 
-        addItemToList(newItem, savedData.length - 1);
+        addItemToList(newItem);
 
         form.reset();
     });
 
-    function addItemToList(item, index) {
+    function addItemToList(item) {
         const li = document.createElement('li');
-        li.innerHTML = `Date: ${item.date}, Store: ${item.store}, Income: ${item.income} 
-                        <button onclick="editItem(${index})">Edit</button>`;
+        li.textContent = `Date: ${item.date}, Store: ${item.store}, Income: ${item.income}`;
         incomeList.appendChild(li);
-    }
-
- /*   
-     function addItemToList(item, index) {
-        const li = document.createElement('li');
-        li.innerHTML = `Date: ${item.date}, Store: ${item.store}, Income: ${item.income} 
-                        <button onclick="editItem(${index})">Edit</button>
-                        <button onclick="deleteItem(${index})">Delete</button>`;
-        incomeList.appendChild(li);
-    }
- 
- window.editItem = function(index) {
-        const item = savedData[index];
-        document.getElementById('store').value = item.store;
-        document.getElementById('income').value = item.income;
-        savedData.splice(index, 1);
-        localStorage.setItem('bookkeepingData', JSON.stringify(savedData));
-        renderList();
-    };
-
-    window.deleteItem = function(index) {
-        savedData.splice(index, 1);
-        localStorage.setItem('bookkeepingData', JSON.stringify(savedData));
-        renderList();
-    };
-*/
-    function renderList() {
-        incomeList.innerHTML = '';
-        savedData.forEach((item, index) => {
-            addItemToList(item, index);
-        });
     }
 });
-
